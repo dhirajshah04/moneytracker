a07cd307b6c38f8ff471bc44490061c68903a476
dhirajshah04 <dhiraj.shah04@gmail.com>
2020-10-28 19:55:57 +0545

    add pages app for dashboards and enduser pages

--- moneytracker/urls.py
+++ moneytracker/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
     path('user/', include('users.urls')),
     path('money/', include('money.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
