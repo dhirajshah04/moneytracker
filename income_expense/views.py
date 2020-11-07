2b0ad52c7ee1e2566ede8d0f9661895f54f61507
dhirajshah04 <dhiraj.shah04@gmail.com>
2020-11-07 10:00:17 +0545

    add generate pdf report for income

--- income_expense/views.py
+++ income_expense/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from income_expense.forms import IncomeForm, ExpenseForm
 from income_expense.models import Income, Expense
 from money.models import Money, Account
+from moneytracker.utils import render_pdf
 from transaction_period.models import TransactionPeriod
 
 
@@ -58,12 +59,19 @@ def income_add(request):
 def income_list(request):
     context = {}
 
+    pdf_report = request.GET.get('pdf_report', '')
+
     income = Income.objects.filter(user=request.user, transaction_period__is_active=True)
     total_income = income.aggregate(Sum('income_amount'))
     transaction_period = TransactionPeriod.get_active_transaction_period(request)
     context['income'] = income
     context['total_income'] = total_income
     context['transaction_period'] = transaction_period
+
+    if pdf_report == 'pdf':
+        return render_pdf('income_expense/income_report_pdf.html', context,
+                          filename='income_report_{}.pdf'.format(transaction_period))
+
     return render(request, 'income_expense/income_list.html', context)
 
 
